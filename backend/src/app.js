255d646fa838e9807d3fc35cf7fb41ee1b53259f
Gabriel Francisco Vaz Azevedo <gabrielvaz2143@gmail.com>
2025-12-14 11:27:06 -0300

    feat: servidor express configurado

---
+++ backend/src/app.js
@@ -0,0 +1,19 @@
+const express = require('express')
+const cors = require('cors')
+
+
+const app = express()
+
+
+app.use(cors())
+app.use(express.json())
+
+
+app.get('/', (req, res) => {
+res.json({ message: 'API rodando' })
+})
+
+
+app.listen(3000, () => {
+console.log('Servidor rodando na porta 3000')
+})
