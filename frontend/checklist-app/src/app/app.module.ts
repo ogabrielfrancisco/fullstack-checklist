9a049abc8d1ee756a788eabc33657bf325e5d529
Gabriel Francisco Vaz Azevedo <gabrielvaz2143@gmail.com>
2025-12-14 11:22:16 -0300

    chore: estrutura inicial do projeto

---
+++ frontend/checklist-app/src/app/app.module.ts
@@ -0,0 +1,24 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { LoginComponent } from './pages/login/login.component';
+import { RegisterComponent } from './pages/register/register.component';
+import { ChecklistComponent } from './pages/checklist/checklist.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    LoginComponent,
+    RegisterComponent,
+    ChecklistComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
