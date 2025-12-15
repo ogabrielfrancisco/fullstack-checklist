af652c8dff4f4a6e7ed0103961c6847e10afe959
Gabriel Francisco Vaz Azevedo <gabrielvaz2143@gmail.com>
2025-12-14 23:45:15 -0300

    feat: configurar rotas protegidas

--- frontend/checklist-app/src/app/app-routing.module.ts
+++ frontend/checklist-app/src/app/app-routing.module.ts
@@ -1,10 +1,23 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './pages/login/login.component';
+import { RegisterComponent } from './pages/register/register.component';
+import { ChecklistComponent } from './pages/checklist/checklist.component';
+import { AuthGuard } from './guards/auth.guard';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: 'login', pathMatch: 'full' },
+  { path: 'login', component: LoginComponent },
+  { path: 'register', component: RegisterComponent },
+  {
+    path: 'checklist',
+    component: ChecklistComponent,
+    canActivate: [AuthGuard]
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
