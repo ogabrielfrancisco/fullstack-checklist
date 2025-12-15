7b6ae80fe7aef17b54cd4db1f31331c1171df7a5
Gabriel Francisco Vaz Azevedo <gabrielvaz2143@gmail.com>
2025-12-14 23:59:04 -0300

    feat: tela de login funcional

--- frontend/checklist-app/src/app/pages/login/login.component.ts
+++ frontend/checklist-app/src/app/pages/login/login.component.ts
@@ -1,10 +1,31 @@
 import { Component } from '@angular/core';
+import { FormBuilder } from '@angular/forms';
+import { Router } from '@angular/router';
+import { AuthService } from '../../services/auth.service';
 
 @Component({
   selector: 'app-login',
-  templateUrl: './login.component.html',
-  styleUrls: ['./login.component.css']
+  templateUrl: './login.component.html'
 })
 export class LoginComponent {
+  form = this.fb.group({
+    email: [''],
+    password: ['']
+  });
 
+  constructor(
+    private fb: FormBuilder,
+    private auth: AuthService,
+    private router: Router
+  ) {}
+
+  submit() {
+    this.auth.login(this.form.value).subscribe({
+      next: (res) => {
+        this.auth.saveToken(res.token);
+        this.router.navigate(['/checklist']);
+      },
+      error: () => alert('Email ou senha inválidos')
+    });
+  }
 }
