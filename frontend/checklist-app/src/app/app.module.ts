cacb135fe49dacb5a47a1aa9d4cd7cd8895d9bd1
Gabriel Francisco Vaz Azevedo <gabrielvaz2143@gmail.com>
2025-12-15 00:02:53 -0300

    feat: checklist completo no frontend

--- frontend/checklist-app/src/app/app.module.ts
+++ frontend/checklist-app/src/app/app.module.ts
@@ -1,11 +1,16 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AppRoutingModule } from './app-routing.module';
+
 import { LoginComponent } from './pages/login/login.component';
 import { RegisterComponent } from './pages/register/register.component';
 import { ChecklistComponent } from './pages/checklist/checklist.component';
+
+import { AuthInterceptor } from './interceptors/auth.interceptor';
 
 @NgModule({
   declarations: [
@@ -16,9 +21,14 @@ import { ChecklistComponent } from './pages/checklist/checklist.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    FormsModule,
+    ReactiveFormsModule
   ],
-  providers: [],
+  providers: [
+    { provide: HTTP_INTERCEPTORS, useClass: AuthInterceptor, multi: true }
+  ],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
--- frontend/checklist-app/src/app/pages/checklist/checklist.component.ts
+++ frontend/checklist-app/src/app/pages/checklist/checklist.component.ts
@@ -1,10 +1,41 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ChecklistService } from '../../services/checklist.service';
+import { AuthService } from '../../services/auth.service';
 
 @Component({
   selector: 'app-checklist',
-  templateUrl: './checklist.component.html',
-  styleUrls: ['./checklist.component.css']
+  templateUrl: './checklist.component.html'
 })
-export class ChecklistComponent {
+export class ChecklistComponent implements OnInit {
+  items: any[] = [];
+  titulo = '';
 
+  constructor(
+    private checklist: ChecklistService,
+    public auth: AuthService
+  ) {}
+
+  ngOnInit() {
+    this.load();
+  }
+
+  load() {
+    this.checklist.list().subscribe(res => this.items = res);
+  }
+
+  add() {
+    this.checklist.create({ titulo: this.titulo }).subscribe(() => {
+      this.titulo = '';
+      this.load();
+    });
+  }
+
+  toggle(item: any) {
+    this.checklist.update(item.id, { concluido: !item.concluido })
+      .subscribe(() => this.load());
+  }
+
+  remove(id: number) {
+    this.checklist.remove(id).subscribe(() => this.load());
+  }
 }
