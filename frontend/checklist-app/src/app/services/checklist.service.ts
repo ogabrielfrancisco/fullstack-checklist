6bae38aedaf806ed6fa98d4082bc499ebbfc20ad
Gabriel Francisco Vaz Azevedo <gabrielvaz2143@gmail.com>
2025-12-14 23:27:00 -0300

    feat: implementar checklist service

--- frontend/checklist-app/src/app/services/checklist.service.ts
+++ frontend/checklist-app/src/app/services/checklist.service.ts
@@ -1,9 +1,31 @@
-import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http'
+import { Injectable } from '@angular/core'
 
-@Injectable({
-  providedIn: 'root'
-})
+
+@Injectable({ providedIn: 'root' })
 export class ChecklistService {
+api = 'http://localhost:3000/checklist'
 
-  constructor() { }
+
+constructor(private http: HttpClient) {}
+
+
+list() {
+return this.http.get<any[]>(`${this.api}/items`)
+}
+
+
+create(data: any) {
+return this.http.post(`${this.api}/items`, data)
+}
+
+
+update(id: number, data: any) {
+return this.http.put(`${this.api}/items/${id}`, data)
+}
+
+
+remove(id: number) {
+return this.http.delete(`${this.api}/items/${id}`)
+}
 }
