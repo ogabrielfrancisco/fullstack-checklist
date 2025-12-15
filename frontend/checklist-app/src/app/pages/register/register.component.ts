24ecc03894b981ecf076ee61b5f0670bb6472d03
Gabriel Francisco Vaz Azevedo <gabrielvaz2143@gmail.com>
2025-12-15 00:00:47 -0300

    feat: tela de cadastro funcional

--- frontend/checklist-app/src/app/pages/register/register.component.ts
+++ frontend/checklist-app/src/app/pages/register/register.component.ts
@@ -1,10 +1,29 @@
 import { Component } from '@angular/core';
+import { FormBuilder } from '@angular/forms';
+import { Router } from '@angular/router';
+import { AuthService } from '../../services/auth.service';
 
 @Component({
   selector: 'app-register',
-  templateUrl: './register.component.html',
-  styleUrls: ['./register.component.css']
+  templateUrl: './register.component.html'
 })
 export class RegisterComponent {
+  form = this.fb.group({
+    name: [''],
+    email: [''],
+    password: ['']
+  });
 
+  constructor(
+    private fb: FormBuilder,
+    private auth: AuthService,
+    private router: Router
+  ) {}
+
+  submit() {
+    this.auth.register(this.form.value).subscribe(() => {
+      alert('Usuário cadastrado');
+      this.router.navigate(['/login']);
+    });
+  }
 }
