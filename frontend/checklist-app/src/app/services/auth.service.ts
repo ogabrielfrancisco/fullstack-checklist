bfb59f06b9c1792d0ef91a3fa83b8a49fa666eb4
Gabriel Francisco Vaz Azevedo <gabrielvaz2143@gmail.com>
2025-12-14 23:26:07 -0300

    feat: implementar auth service

--- frontend/checklist-app/src/app/services/auth.service.ts
+++ frontend/checklist-app/src/app/services/auth.service.ts
@@ -1,9 +1,38 @@
-import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http'
+import { Injectable } from '@angular/core'
+import { Router } from '@angular/router'
 
-@Injectable({
-  providedIn: 'root'
-})
+
+@Injectable({ providedIn: 'root' })
 export class AuthService {
+api = 'http://localhost:3000/auth'
 
-  constructor() { }
+
+constructor(private http: HttpClient, private router: Router) {}
+
+
+login(data: any) {
+return this.http.post<any>(`${this.api}/login`, data)
+}
+
+
+register(data: any) {
+return this.http.post(`${this.api}/register`, data)
+}
+
+
+saveToken(token: string) {
+localStorage.setItem('token', token)
+}
+
+
+isLogged() {
+return !!localStorage.getItem('token')
+}
+
+
+logout() {
+localStorage.removeItem('token')
+this.router.navigate(['/login'])
+}
 }
