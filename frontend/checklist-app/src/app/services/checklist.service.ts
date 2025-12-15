96602610b9db87a46c9a3d8bf5124d315fa1924d
Gabriel Francisco Vaz Azevedo <gabrielvaz2143@gmail.com>
2025-12-15 01:53:49 -0300

    fix: alinhar rotas e controller do checklist

--- frontend/checklist-app/src/app/services/checklist.service.ts
+++ frontend/checklist-app/src/app/services/checklist.service.ts
@@ -1,31 +1,25 @@
 import { HttpClient } from '@angular/common/http'
 import { Injectable } from '@angular/core'
 
-
 @Injectable({ providedIn: 'root' })
 export class ChecklistService {
-api = 'http://localhost:3000/checklist'
+  private api = 'http://localhost:3000/checklist'
 
+  constructor(private http: HttpClient) {}
 
-constructor(private http: HttpClient) {}
+  list() {
+    return this.http.get<any[]>(this.api)
+  }
 
+  create(data: any) {
+    return this.http.post(this.api, data)
+  }
 
-list() {
-return this.http.get<any[]>(`${this.api}/items`)
-}
+  update(id: number, data: any) {
+    return this.http.put(`${this.api}/${id}`, data)
+  }
 
-
-create(data: any) {
-return this.http.post(`${this.api}/items`, data)
-}
-
-
-update(id: number, data: any) {
-return this.http.put(`${this.api}/items/${id}`, data)
-}
-
-
-remove(id: number) {
-return this.http.delete(`${this.api}/items/${id}`)
-}
+  remove(id: number) {
+    return this.http.delete(`${this.api}/${id}`)
+  }
 }
